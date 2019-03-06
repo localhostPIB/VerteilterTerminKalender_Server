d0c0481ae9274e5d6d44a11c587902c27e2b5cb2
KerimZ <33584276+KerimZ@users.noreply.github.com>
2019-03-06 15:55:50 +0100

    Server implemented

---
+++ src/main/java/terminkalender/server/ServiceLauncher.java
@@ -0,0 +1,28 @@
+package terminkalender.server;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class ServiceLauncher {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        String baseUrl = (args.length > 0) ? args[0] : "http://localhost:8000";
+        ResourceConfig rc = new ResourceConfig();
+        rc.registerInstances(new UserRessource());
+
+        final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(baseUrl), rc, false);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                server.shutdownNow();
+            }
+        }));
+        server.start();
+        System.out.println("Server start");
+        Thread.currentThread().join();
+    }
+}
