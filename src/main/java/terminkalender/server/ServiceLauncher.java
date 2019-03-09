40c7cb6b7c7657105cb992bd2931f761fd262641
KerimZ <33584276+KerimZ@users.noreply.github.com>
2019-03-09 14:26:22 +0100

    Service Classes of Endpoints included in Ressourceinstancelist of Server. Endpoints should be accessible through http now.

--- src/main/java/terminkalender/server/ServiceLauncher.java
+++ src/main/java/terminkalender/server/ServiceLauncher.java
@@ -3,15 +3,20 @@ package terminkalender.server;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import terminkalender.service.classes.*;
 
 import java.io.IOException;
 import java.net.URI;
 
+/**
+ *
+ */
 public class ServiceLauncher {
     public static void main(String[] args) throws InterruptedException, IOException {
         String baseUrl = (args.length > 0) ? args[0] : "http://localhost:8000";
         ResourceConfig rc = new ResourceConfig();
-        rc.registerInstances(new UserRessource());
+        //rc.registerInstances(new UserRessource(), new UserRessource2()); //TODO: DELETE THIS LINE AFTER TESTING
+        rc.registerInstances(new EventDeclineServiceImpl(), new EventInviteServiceImpl(), new EventPaticipateServiceImpl(), new EventServiceImpl(), new UserServiceImpl());
 
         final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(baseUrl), rc, false);
 
@@ -22,6 +27,7 @@ public class ServiceLauncher {
             }
         }));
         server.start();
+
         System.out.println("Server start");
         Thread.currentThread().join();
     }
