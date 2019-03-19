3757620d8368a696ebed28c182ba15f446a0e87f
Shenna Risqianto Wilfred Piri <shennawew@outlook.com>
2019-03-19 16:37:23 +0100

    JsonDeseriealize for EventParticipate

--- src/main/java/terminkalender/model/interfaces/EventParticipate.java
+++ src/main/java/terminkalender/model/interfaces/EventParticipate.java
@@ -1,5 +1,9 @@
 package terminkalender.model.interfaces;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import terminkalender.model.classes.EventParticipateImpl;
+
+@JsonDeserialize(as = EventParticipateImpl.class)
 public interface EventParticipate
 {
 	//----------------PARTICIPATEID----------------
